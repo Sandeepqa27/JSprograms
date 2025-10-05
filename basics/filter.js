a2a7dbede1a9d3f177fb8235c91d8c472ecc83f5
sandeep Kumar <appiuma2z@gmail.com>
2025-10-05 11:38:39 +0530

    seperate reduce , map and filter method

--- basics/filter.js
+++ basics/filter.js
@@ -1,16 +1,5 @@
 let secondpumarks=[84,82,92,98,55,66,77]
 
-var sum=0;
-for(let i=0;i<secondpumarks.length;i++){
-   console.log(i);
-sum+=secondpumarks[i]; //sum=sum+secondpumarks[i]
-//     sum=sum+secondpumarks[i]
-}
-console.log(sum);
-//reduce,filter ,map
-secondpumarks.reduce((sum,marks)=>sum+marks,0)
-console.log("Sandeep got total of this much in 2nd pu"+sum);
-
 console.log("filter even numbers using traditional way");
 var scores=[12,13,14,16]
 var evenscores=[]
@@ -27,6 +16,3 @@ var oddscores=scores.filter(marks=>marks%2!=0)
 console.log(oddscores);
 
 
-//map
-var newmarks=scores.map(marks=>marks+5)
-console.log(newmarks);
---
+++ basics/map.js
@@ -0,0 +1,5 @@
+var scores=[12,13,14,16]
+
+//map
+var newscore=scores.map(marks=>marks+5)
+console.log(newscore);
